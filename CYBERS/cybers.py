30d2150b42e3acdc3f15094a4b2752da3cd34d37
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-12-01 00:45:34 -0800

    Create cybers.py

---
+++ CYBERS/cybers.py
@@ -0,0 +1,6 @@
+cybers import legendx
+cybers import invisibles
+cybers import team-cybers
+cybers from git=1.99.0
+git=("https://github.com/INVISIBLE-CYBER-BUCK/CYBERBUCKUSERBOT/new/master/CYBERS"|| BRANCH ||
+     REPO($UPSTRING):
