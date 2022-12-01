a480f81b622e91a093d52f45e191ed74d7b6bb5a
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-12-01 00:35:39 -0800

    CYBERS

--- CyberString.py
+++ CyberString.py
@@ -15,7 +15,7 @@ if okvai == "69":
         client.send_message("me", client.session.save())
         client.send_message(
             "me",
-            "Above is your #LEGENDBOT STRING SESSION \nPaste this string in Heroku Var.\n\n[Team LegendBot](t.me/LegendBot_AI)",
+            "Above is your #CYBERBOT STRING SESSION \nPaste this string in Heroku Var.\n\n[Team CyberBuck](t.me/invisiblesecuritycyberbuckchat)",
         )
 
 else:
