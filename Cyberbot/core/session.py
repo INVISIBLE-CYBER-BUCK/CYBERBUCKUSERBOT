65f062d9e53bf66498b2cc18b3c13a77f6c0ed30
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-12-01 01:31:55 -0800

    Update session.py

--- Cyberbot/core/session.py
+++ Cyberbot/core/session.py
@@ -10,10 +10,10 @@ __version__ = "1.10.6"
 
 loop = None
 
-if Config.LEGEND_STRING:
-    session = StringSession(str(Config.LEGEND_STRING))
+if Config.CYBER_STRING:
+    session = StringSession(str(Config.CYBER_STRING))
 else:
-    session = "LegendUserBot"
+    session = "CyberUserBot"
 
 try:
     legend = LegendClient(
@@ -27,7 +27,7 @@ try:
         connection_retries=None,
     )
 except Exception as e:
-    print(f"LEGEND_STRING - {e}")
+    print(f"CYBER_STRING - {e}")
     sys.exit()
 
 legend.tgbot = tgbot = LegendClient(
