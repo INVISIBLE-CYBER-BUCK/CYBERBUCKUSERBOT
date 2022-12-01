1cdf1ae7e7ae9133d5e69219b0c140adcbc77ca5
INVISIBLE-CYBER-BUCK <119512230+INVISIBLE-CYBER-BUCK@users.noreply.github.com>
2022-12-01 00:24:35 -0800

    Create cyber.js

---
+++ Cyberbot/cyber.js
@@ -0,0 +1,4 @@
+from Legendbot import all_plugins.helpers
+       import= Cyberbot.all_plugins.deletes).Legendbot
+                LegendBot in Cyberbot class :
+                    class Cyberbot def import plugins, _all-plugins, _all_plugins, Legendbot(Cyberbot):
