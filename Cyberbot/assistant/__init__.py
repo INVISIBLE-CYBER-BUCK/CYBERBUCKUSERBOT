e31f6da8bbc7d0b3190babd0ca2dcf64515483dd
INVISIBLE-CYBER-BUCK <119512230+INVISIBLE-CYBER-BUCK@users.noreply.github.com>
2022-12-01 00:28:29 -0800

    Add files via upload

---
+++ Cyberbot/assistant/__init__.py
@@ -0,0 +1,9 @@
+from Cyberbot import BOTLOG, BOTLOG_CHATID
+from Cyberbot.core.session import legend, tgbot
+
+from ..Config import Config
+from ..core.inlinebot import *
+
+LEGEND_USER = legend.me.first_name
+The_LegendBoy = legend.uid
+legend_mention = f"[{LEGEND_USER}](tg://user?id={The_LegendBoy})"
